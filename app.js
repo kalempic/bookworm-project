d1f25186f04203b30ff511bce4ae2de8c48584bd
Katarina Alempić <39443702+kalempic@users.noreply.github.com>
2022-09-19 22:43:06 +0200

    Added models and some controllers

---
+++ app.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const Books = require('./models/books');
+const books_import = require('./booksDatabase');
+
+mongoose.connect('mongodb://localhost:27017/booksdb',{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+//ako je uspesno otvoreno tj radi ili ako ne radi
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize({
+    replaceWith: '_'
+}))
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
+})
+
---
+++ models/books.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Review = require('./review')
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+    title: String,
+    description: String,
+    location: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+});
+
+
+BookSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
+
+module.exports = mongoose.model('Book', BookSchema);
